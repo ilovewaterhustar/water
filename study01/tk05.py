f1730a7efa28b6023fc92b3d59e72bd75eaf50bc
kimmina <kimmina.water@gmail.com>
2021-12-06 13:10:38 +0900

    예제

---
+++ study01/tk05.py
@@ -0,0 +1,12 @@
+from tkinter import *
+w=Tk()
+
+b1=Button(w, text = "박스 #1",bg="red",fg="white")
+b2=Button(w, text = "박스 #1",bg="green",fg="white")
+b3=Button(w, text = "박스 #1",bg="orange",fg="white")
+b4=Button(w, text = "박스 #1",bg="pink",fg="white")
+
+b1.grid(row=0,column=0)
+b2.grid(row=0,column=0)
+b3.grid(row=0,column=0)
+b4.grid(row=0,column=0)
